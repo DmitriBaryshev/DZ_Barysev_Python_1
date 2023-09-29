29e22a174d356af88d543aad0bf3e8d70b191939
Dmitry Baryshev <Stud@sar64.local>
2023-09-29 21:22:12 +0400

    DZ_Barysev_1

---
+++ DZ.py
@@ -0,0 +1,18 @@
+# Задание 1
+a = int(input())
+b = int(input())
+c = int(input())
+print("Сумма = ", a + b + c, '\n' "Произведение = ", a * b * c)
+# Задание 2
+a = int(input())
+b = int(input())
+c = int(input())
+print("Остаток денег = ", abs(a - (b + c)))
+# Задание 3
+AC = int(input())
+BD = int(input())
+print("S = ", (AC * BD)/2)
+# Задание 4
+print("To be", '\n' "or not", '\n' "to be")
+# Задание 5
+print("Life is what happens", '\n\t' "when", '\n\t\t' "you're busy making other plans", '\n\t\t\t\t\t\t\t\t\t' "John Lennon")
